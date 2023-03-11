3211b4652e59fe0a0c197df563f6394f95ff6dcc
chas <2412798007@qq.com>
2023-03-11 09:33:13 +0800

    合作方展示

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/permit/PartnerAnController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/permit/PartnerAnController.java
@@ -3,6 +3,7 @@ package com.ruoyi.web.controller.system.permit;
 import com.ruoyi.common.annotation.Anonymous;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.system.domain.SysPartner;
 import com.ruoyi.system.service.ISysPartnerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,7 @@ public class PartnerAnController extends BaseController {
     private ISysPartnerService sysPartnerService;
 
     /**
-     * 随机获取4个合作方 展示
+     * 随机获取5个合作方 展示
      * @return 列表
      */
     @Anonymous
@@ -34,7 +35,7 @@ public class PartnerAnController extends BaseController {
     public AjaxResult getShow(){
         List<SysPartner> newList = new ArrayList<>();
         List<SysPartner> partners = sysPartnerService.selectSysPartnerList(new SysPartner());
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
             int size = partners.size();
             Random random = new Random();
             int num = random.nextInt(size);
@@ -43,5 +44,12 @@ public class PartnerAnController extends BaseController {
             newList.add(partner);
         }
         return AjaxResult.success(newList);
+    }
+
+    @Anonymous
+    @PostMapping("/getAll")
+    public TableDataInfo getAll(Integer pageNum,Integer pageSize){
+        startPage();
+        return getDataTable(sysPartnerService.selectSysPartnerList(new SysPartner()));
     }
 }

1fa870b449b284cf215298c137848e20765491f7
chas <2412798007@qq.com>
2023-03-11 09:08:01 +0800

    合作方展示

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/permit/PartnerAnController.java
@@ -0,0 +1,47 @@
+package com.ruoyi.web.controller.system.permit;
+
+import com.ruoyi.common.annotation.Anonymous;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.system.domain.SysPartner;
+import com.ruoyi.system.service.ISysPartnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author chas
+ * @introduction  合作方
+ * @data 2023-3
+ */
+@RestController
+@RequestMapping("/partner")
+public class PartnerAnController extends BaseController {
+    @Autowired
+    private ISysPartnerService sysPartnerService;
+
+    /**
+     * 随机获取4个合作方 展示
+     * @return 列表
+     */
+    @Anonymous
+    @PostMapping("/getShow")
+    public AjaxResult getShow(){
+        List<SysPartner> newList = new ArrayList<>();
+        List<SysPartner> partners = sysPartnerService.selectSysPartnerList(new SysPartner());
+        for (int i = 0; i < 4; i++) {
+            int size = partners.size();
+            Random random = new Random();
+            int num = random.nextInt(size);
+            SysPartner partner = partners.get(num);
+            partners.remove(num);
+            newList.add(partner);
+        }
+        return AjaxResult.success(newList);
+    }
+}

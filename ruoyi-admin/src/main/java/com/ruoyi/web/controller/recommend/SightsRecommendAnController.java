e8fb0fd567157a45924857cdd888a34736c5591e
chas <2412798007@qq.com>
2023-04-14 21:00:37 +0800

    (2023-4-14 9:00)最后一个业务哦 Chas 回顾一年 小池~~

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/recommend/SightsRecommendAnController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/recommend/SightsRecommendAnController.java
@@ -1,11 +1,13 @@
 package com.ruoyi.web.controller.recommend;
 
 import com.ruoyi.common.annotation.Anonymous;
+import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.recommend.service.SightsRecommendService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Api("推荐算法对外展示接口")
 @RestController
 @RequestMapping("/recommend")
-public class SightsRecommendAnController {
+public class SightsRecommendAnController extends BaseController {
 
     @Autowired
     private SightsRecommendService recommendService;
+
+
 
     @ApiOperation("相似景点")
     @Anonymous()
@@ -30,4 +34,21 @@ public class SightsRecommendAnController {
     public AjaxResult SimilarAttractions(@PathVariable Long sightsId){
         return AjaxResult.success(recommendService.SimilarAttractions(sightsId));
     }
+
+    @ApiOperation("实时推荐")
+    @PreAuthorize("@ss.hasRole('common')")
+    @GetMapping("/online")
+    public AjaxResult onlineRecommend(){
+        return AjaxResult.success(recommendService.onlineAttractions(getUserId()));
+    }
+
+    @ApiOperation("普通推荐")
+    @Anonymous
+    @GetMapping("/common")
+    public AjaxResult commonRecommend(){
+
+        return AjaxResult.success(recommendService.getRandomAttractions());
+    }
+
+
 }
